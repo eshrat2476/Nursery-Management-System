e3dc999b61874d3c8c743e6054cdf02453e8ec82
eshrat <eshrataraupoma@gmail.com>
2023-12-06 15:06:50 +0600

    Plants Design  Update

--- resources/views/Frontend/Pages/Plants/Plants.blade.php
+++ resources/views/Frontend/Pages/Plants/Plants.blade.php
@@ -3,11 +3,19 @@
 @section('content')
 
 
-<div class="container">
 
-    <h1>𓂀𝕆𝕦𝕣 ℙ𝕝𝕒𝕟𝕥𝕤𓂀</h1>
-    <span>𝑀𝑜𝓇𝑒 𝒫𝓁𝒶𝓃𝓉𝓈 𝐿𝑒𝓈𝓈 𝒫𝓇𝑜𝒷𝓁𝑒𝓂𝓈</span>
+<div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeIn;">
+    <div class="container text-center py-5">
+        <h1 class="display-3 text-white mb-4  slideInDown">𓂀𝕆𝕦𝕣 ℙ𝕝𝕒𝕟𝕥𝕤𓂀</h1>
+        <span style="color:white">𝑀𝑜𝓇𝑒 𝒫𝓁𝒶𝓃𝓉𝓈 𝐿𝑒𝓈𝓈 𝒫𝓇𝑜𝒷𝓁𝑒𝓂𝓈</span>
 
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb justify-content-center mb-0">
+                <li class="breadcrumb-item"><a href="{{route('Home')}}">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Plants</li>
+            </ol>
+        </nav>
+    </div>
 </div>
 
 
@@ -72,16 +80,16 @@
 
                     <h5 class="card-title font-weight-bold">{{$Plant_item->plantname}}</h5>
                     <!-- Product reviews-->
-                        <div class="d-flex justify-content-center small text-warning mb-2">
-                            <div class="bi-star-fill"></div>
-                            <div class="bi-star-fill"></div>
-                            <div class="bi-star-fill"></div>
-                            <div class="bi-star-fill"></div>
-                            <div class="bi-star-fill"></div>
-                        </div>
+                    <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                    </div>
 
 
-                    <p class="card-text">Tk {{$Plant_item->plantprice}}  .BDT</p>
+                    <p class="card-text">Tk {{$Plant_item->plantprice}} .BDT</p>
                     <a href="#" class="btn details px-auto">view details</a><br />
                     <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{route('add_toCart',$Plant_item->id)}}">Add to cart</a></div>
                 </div>
